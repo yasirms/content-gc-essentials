a6cb55d874fca02f6fe770cf26890a18874b7668
yasirms <yasirms.al@gmail.com>
2019-06-02 15:40:48 +0200

    Update app.py

--- gke-lab-03/canary/app.py
+++ gke-lab-03/canary/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def showPinehead():
-    html = "<div style='text-align:center;margin:20px;'><h1>Greetings from Linux Academy!</h1><h2 style='color: #ffef00; text-shadow: 2px 2px 3px #030000'>Canary Version</h2><img src='https://storage.googleapis.com/la-gcp-labs-resources/essentials/Logo-Pinehead-NVY.png' width='40%' alt='Pinehead @ Linux Academy'></div>"
+    html = "<div style='text-align:center;margin:20px;'><h1>Greetings from Yasir Mehmood!</h1><h2 style='color: #ffef00; text-shadow: 2px 2px 3px #030000'>Canary Version - Apple iPhone XR</h2><img src='https://1540481566.rsc.cdn77.org/imagecache/apple-iphone-xr-64gb-black-front.png' width='40%' alt='Canary Version'></div>"
     return html
 
 if __name__ == "__main__":

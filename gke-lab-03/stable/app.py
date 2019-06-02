54db481080b82420102b883a7e29fcf0fb5089a8
yasirms <yasirms.al@gmail.com>
2019-06-02 15:42:11 +0200

    Update app.py

--- gke-lab-03/stable/app.py
+++ gke-lab-03/stable/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def showPinehead():
-    html = "<div style='text-align:center;margin:20px;'><h1>Greetings from Linux Academy!</h1><h2>Stable Version</h2><img src='https://storage.googleapis.com/la-gcp-labs-resources/essentials/Logo-Pinehead-NVY.png' width='40%' alt='Pinehead @ Linux Academy'></div>"
+    html = "<div style='text-align:center;margin:20px;'><h1>Greetings from Yasir Mehmood!</h1><h2>Stable Version</h2><img src='https://1540481566.rsc.cdn77.org/imagecache/samsung-galaxy-s10e-128gb-prism-black-front-20190520144320.png' width='40%' alt='Stable Version'></div>"
     return html
 
 if __name__ == "__main__":
